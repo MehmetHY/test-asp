c2494eda63af471094bfb0d5c5152de58d3e66ac
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-31 22:54:14 +0300

    created ITodoRepository Interface

--- DbAccess/Repo/Interfaces/ICategoryRepository.cs
+++ DbAccess/Repo/Interfaces/ICategoryRepository.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TodoModels.Models;
+﻿using TodoModels.Models;
 
 namespace DbAccess.Repo.Interfaces
 {
     public interface ICategoryRepository : IRepository<CategoryModel>
     {
         Task Update(CategoryModel category);
-        Task Exist(CategoryModel category);
-        Task Exist(int id);
-        Task Exist(string name);
+        Task Any(CategoryModel category);
+        Task Any(int id);
+        Task Any(string name);
         Task<IEnumerable<CategoryModel>> GetCategoriesOfUser(UserModel user);
         Task<IEnumerable<CategoryModel>> GetSubCategories(CategoryModel category);
     }
---
+++ DbAccess/Repo/Interfaces/ITodoRepository.cs
@@ -0,0 +1,12 @@
+﻿using TodoModels.Models;
+
+namespace DbAccess.Repo.Interfaces
+{
+    public interface ITodoRepository  : IRepository<TodoModel>
+    {
+        Task Update(TodoModel todo);
+        Task<bool> Any(TodoModel todo);
+        Task<bool> Any(int id);
+        Task<IEnumerable<TodoModel>> GetTodosOfCategory(int id);
+    }
+}
