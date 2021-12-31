02dcda61907c7b171744b4ad526e540ca1349ddc
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-31 20:35:45 +0300

    created todo modle

--- TodoModels/Models/TodoModel.cs
+++ TodoModels/Models/TodoModel.cs
@@ -1,6 +1,22 @@
-﻿namespace TodoModels.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoModels.Models
 {
     internal class TodoModel
     {
+        public int? Id { get; set; }
+
+        public CategoryModel? Category { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required]
+        [MaxLength(64)]
+        public string? Title { get; set; }
+
+        [DataType(DataType.Text)]
+        public string? Description { get; set; }
+
+        public enum TodoState { NotStarted, InProgress, Completed } 
+        public TodoState State { get; set; } = TodoState.NotStarted;
     }
 }
