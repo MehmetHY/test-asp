49f293871e29c755f5d97a2ddbd37766d9116af0
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-31 20:25:01 +0300

    created User model

--- DbAccess/Repo/Interfaces/IRepository.cs
+++ DbAccess/Repo/Interfaces/IRepository.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
 
-namespace DbAccess.Repo
+namespace DbAccess.Repo.Interfaces
 {
     public interface IRepository<T> where T : class
     {
---
+++ TodoModels/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoModels.Models
+{
+    internal class UserModel
+    {
+        public int? Id { get; set; }
+        
+        [DataType(DataType.Text)]
+        [Required]
+        [MaxLength(64)]
+        public string? Name { get; set; }
+        
+        [DataType(DataType.Password)]
+        [Required]
+        [MaxLength(64)]
+        public string? Password { get; set; }
+    }
+}
