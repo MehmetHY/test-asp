aa9a516f7d8b14cddda398a0096964033f8b5050
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-31 20:10:17 +0300

    created IDbContext and MySqlDbContext

---
+++ DbAccess/Data/Interfaces/IDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAccess.Data.Interfaces
+{
+    public interface IDbContext
+    {
+        string ConnectionString { get; }
+        Task Execute<T>(string sql, T parameters);
+        Task<IEnumerable<T>> Query<T, U>(string comsqlmand, U parameters);
+    }
+}
