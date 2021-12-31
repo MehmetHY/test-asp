8c72b9e0e1b9d2cc751b28de6158b9c0bf72f0a5
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-31 22:23:43 +0300

    created ICategoryRepository interface

---
+++ DbAccess/Repo/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoModels.Models;
+
+namespace DbAccess.Repo.Interfaces
+{
+    public interface ICategoryRepository : IRepository<CategoryModel>
+    {
+        Task Update(CategoryModel category);
+        Task Exist(CategoryModel category);
+        Task Exist(int id);
+        Task Exist(string name);
+        Task<IEnumerable<CategoryModel>> GetCategoriesOfUser(UserModel user);
+        Task<IEnumerable<CategoryModel>> GetSubCategories(CategoryModel category);
+    }
+}
--- DbAccess/Repo/Interfaces/IUserRepository.cs
+++ DbAccess/Repo/Interfaces/IUserRepository.cs
@@ -4,7 +4,8 @@ namespace DbAccess.Repo.Interfaces
 {
     public interface IUserRepository : IRepository<UserModel>
     {
-        Task Update(UserModel model);
+        Task Update(UserModel user);
+        Task<bool> Exists(UserModel user);
         Task<bool> Exists(int id);
         Task<bool> Exists(string name);
     }
