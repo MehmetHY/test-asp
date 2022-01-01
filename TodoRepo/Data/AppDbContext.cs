18dbe3ea43eebd70939f5f5774b54aa6faa38032
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-01 16:37:22 +0300

    created AppDbContext class

---
+++ TodoRepo/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoModels.Models;
+
+namespace TodoRepo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<CategoryModel> Categories { get; set; }
+        public DbSet<TodoModel> Todos { get; set; }
+    }
+}
--- TodoRepo/Repositories/Interfaces/ICategoryRepository.cs
+++ TodoRepo/Repositories/Interfaces/ICategoryRepository.cs
@@ -6,8 +6,6 @@ namespace TodoRepo.Repositories.Interfaces
     public interface ICategoryRepository : IRepository<CategoryModel>
     {
         Task Update(CategoryModel category);
-        Task Any(CategoryModel category);
-        Task Any(int id);
         Task Any(string name);
         Task<IEnumerable<CategoryModel>> GetCategoriesOfUser(UserModel user);
         Task<IEnumerable<CategoryModel>> GetSubCategories(CategoryModel category);
