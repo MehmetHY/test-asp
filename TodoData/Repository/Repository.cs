ddce89a1030d9b3ff5b17dcb22917a7709008ae9
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-02 22:25:47 +0300

    created user repository

---
+++ TodoData/Repository/Repository.cs
@@ -0,0 +1,88 @@
+﻿using Dapper;
+using System.Dynamic;
+using TodoData.Data.Interfaces;
+using TodoData.Repository.Interface;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoData.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly IProcedureCaller _pc;
+        public Repository(IProcedureCaller pc)
+        {
+            _pc = pc;
+        }
+
+        private bool ShouldProcess()
+        {
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                if (prop.CustomAttributes.Any(attrib => attrib.AttributeType == typeof(NotMappedAttribute)))
+                { 
+                    return false; 
+                }
+                if (prop.CustomAttributes.Any(attrib => attrib.AttributeType == typeof(KeyAttribute)))
+                {
+                    return false;
+                }
+                if (prop.Name == "Id")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void Add(T entity)
+        {
+            var parameters = new DynamicParameters();
+            foreach (var prop in entity.GetType().GetProperties())
+            {
+                if (!ShouldProcess()) continue;
+                if (prop.Name != "Id")
+                    parameters.Add(prop.Name, prop.GetValue(entity));
+            }
+            _pc.Execute("Add", parameters);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            foreach (var entity in entities)
+            {
+                Add(entity);
+            }
+        }
+
+        public T? Get(int id)
+        {
+            
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TodoData/Repository/UserRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TodoData.Data.Interfaces;
+using TodoData.Repository.Interface;
+using TodoModels.Models;
+using Dapper;
+
+namespace TodoData.Repository
+{
+    public class UserRepository : IRepository<UserModel>
+    {
+        private readonly IProcedureCaller _pc;
+        public UserRepository(IProcedureCaller pc)
+        {
+            _pc = pc;
+        }
+        
+        public void Add(UserModel entity)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("name", entity.Name, System.Data.DbType.String, System.Data.ParameterDirection.Input, 64);
+            param.Add("password", entity.Password, System.Data.DbType.String, System.Data.ParameterDirection.Input, 64);
+            _pc.Execute("sp_user_add", param);
+            foreach (var category in entity.Categories)
+            {
+                // todo: add categories
+            }
+        }
+
+        public void AddRange(IEnumerable<UserModel> entities)
+        {
+            foreach (var entity in entities)
+            {
+                Add(entity);
+            }
+        }
+
+        public UserModel? Get(int id)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("id", id, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
+            var entity = _pc.GetRow<UserModel>("sp_user_get_by_id", param);
+            if (entity != null)
+            {
+                // todo: fill up category list
+            }
+            return entity;
+        }
+
+        public IEnumerable<UserModel> GetAll()
+        {
+            var users = _pc.GetRows<UserModel>("sp_user_get_all");
+            foreach (var user in users)
+            {
+                // todo: fill up category list
+            }
+            return users;
+        }
+
+        public void Remove(int id)
+        {
+            var param = new DynamicParameters();
+            param.Add("id", id, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
+            _pc.Execute("sp_user_remove_by_id", param);
+        }
+
+        public void Remove(UserModel entity)
+        {
+            Remove(entity.Id ?? -1);
+        }
+
+        public void RemoveAll()
+        {
+            _pc.Execute("sp_user_remove_all");
+        }
+
+        public void RemoveRange(IEnumerable<UserModel> entities)
+        {
+            foreach (var entity in entities)
+            {
+                Remove(entity);
+            }
+        }
+    }
+}
