dcf0874583d003442a4ca6625b6d88d0b10c7ae5
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-02 15:13:14 +0300

    reset project

--- TodoModels/Models/CategoryModel.cs
+++ TodoModels/Models/CategoryModel.cs
@@ -6,15 +6,15 @@ namespace TodoModels.Models
     {
         public int? Id { get; set; }
 
+        [DataType(DataType.Text)]
+        [Required]
+        [MaxLength(64)]
+        public string? Name { get; set; }
+
         public UserModel? User { get; set; }
 
         public List<CategoryModel> SubCategories { get; set; } = new();
 
         public List<TodoModel> Todos { get; set; } = new();
-
-        [DataType(DataType.Text)]
-        [Required]
-        [MaxLength(64)]
-        public string? Name { get; set; }
     }
 }
