3c6dda390e7fee8487a083fcbd4b91fb8edd461b
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-02 22:37:48 +0300

    created todo repository

---
+++ TodoData/Repository/TodoRepository.cs
@@ -0,0 +1,73 @@
+﻿using TodoData.Data.Interfaces;
+using TodoData.Repository.Interface;
+using TodoModels.Models;
+using Dapper;
+
+namespace TodoData.Repository
+{
+    public class TodoRepository : IRepository<TodoModel>
+    {
+        private readonly IProcedureCaller _pc;
+        public TodoRepository(IProcedureCaller pc)
+        {
+            _pc = pc;
+        }
+        
+        public void Add(TodoModel entity)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("Title", entity.Title, System.Data.DbType.String, System.Data.ParameterDirection.Input, 64);
+            param.Add("Description", entity.Description, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+            param.Add("State", (int)entity.State, System.Data.DbType.Int16, System.Data.ParameterDirection.Input);
+            param.Add("Index", entity.Index, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
+            _pc.Execute("sp_todo_add", param);
+        }
+
+        public void AddRange(IEnumerable<TodoModel> entities)
+        {
+            foreach (var entity in entities)
+            {
+                Add(entity);
+            }
+        }
+
+        public TodoModel? Get(int id)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("Id", id, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
+            var entity = _pc.GetRow<TodoModel>("sp_todo_get_by_id", param);
+            return entity;
+        }
+
+        public IEnumerable<TodoModel> GetAll()
+        {
+            var todos = _pc.GetRows<TodoModel>("sp_todo_get_all");
+            return todos;
+        }
+
+        public void Remove(int id)
+        {
+            var param = new DynamicParameters();
+            param.Add("Id", id, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
+            _pc.Execute("sp_todo_remove_by_id", param);
+        }
+
+        public void Remove(TodoModel entity)
+        {
+            Remove(entity.Id ?? -1);
+        }
+
+        public void RemoveAll()
+        {
+            _pc.Execute("sp_todo_remove_all");
+        }
+
+        public void RemoveRange(IEnumerable<TodoModel> entities)
+        {
+            foreach (var entity in entities)
+            {
+                Remove(entity);
+            }
+        }
+    }
+}
--- TodoData/Repository/UserRepository.cs
+++ TodoData/Repository/UserRepository.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using TodoData.Data.Interfaces;
+﻿using TodoData.Data.Interfaces;
 using TodoData.Repository.Interface;
 using TodoModels.Models;
 using Dapper;
@@ -19,10 +16,10 @@ namespace TodoData.Repository
         public void Add(UserModel entity)
         {
             DynamicParameters param = new DynamicParameters();
-            param.Add("name", entity.Name, System.Data.DbType.String, System.Data.ParameterDirection.Input, 64);
-            param.Add("password", entity.Password, System.Data.DbType.String, System.Data.ParameterDirection.Input, 64);
+            param.Add("Name", entity.Name, System.Data.DbType.String, System.Data.ParameterDirection.Input, 64);
+            param.Add("Password", entity.Password, System.Data.DbType.String, System.Data.ParameterDirection.Input, 64);
             _pc.Execute("sp_user_add", param);
-            foreach (var category in entity.Categories)
+            foreach (var category in entity.Categories ?? new List<CategoryModel>())
             {
                 // todo: add categories
             }
@@ -39,7 +36,7 @@ namespace TodoData.Repository
         public UserModel? Get(int id)
         {
             DynamicParameters param = new DynamicParameters();
-            param.Add("id", id, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
+            param.Add("Id", id, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
             var entity = _pc.GetRow<UserModel>("sp_user_get_by_id", param);
             if (entity != null)
             {
@@ -61,7 +58,7 @@ namespace TodoData.Repository
         public void Remove(int id)
         {
             var param = new DynamicParameters();
-            param.Add("id", id, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
+            param.Add("Id", id, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
             _pc.Execute("sp_user_remove_by_id", param);
         }
 
