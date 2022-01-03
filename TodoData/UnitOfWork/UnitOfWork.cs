c8688501c95998dc48be196e4ef020d6ccc0a4ed
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-03 18:38:46 +0300

    created unit of work class

---
+++ TodoData/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using DbAccess.Data;
+using TodoData.Repository;
+
+namespace TodoData.UnitOfWork
+{
+    public class UnitOfWork
+    {
+        public UserRepository UserRepo { get; }
+        public UnitOfWork(IProcedureCaller pc)
+        {
+            UserRepo = new UserRepository(pc);
+        }
+    }
+}
