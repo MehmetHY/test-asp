1510282f029935e91238503f0b5817708d8ebc83
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-04 01:32:02 +0300

    created hashutils

---
+++ TodoApp/Utils/HashUtil.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace TodoApp.Utils
+{
+    public static class HashUtil
+    {
+        public static void ToHashSha256(this string? str)
+        {
+            if (str == null) return;
+            using var sha256 = SHA256.Create();
+            var chars = sha256.ComputeHash(Encoding.UTF8.GetBytes(str));
+            str = chars.ToString();
+        }
+    }
+}
