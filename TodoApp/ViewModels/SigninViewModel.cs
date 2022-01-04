26cda298937a06543df4d8544252103cb25ab027
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-04 13:39:47 +0300

    updated sign in and up view models

--- TodoApp/ViewModels/SigninViewModel.cs
+++ TodoApp/ViewModels/SigninViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
+using TodoApp.ModelBinders;
 
 namespace TodoModels.ViewModels
 {
@@ -9,11 +10,13 @@ namespace TodoModels.ViewModels
         [StringLength(64, ErrorMessage = "Name must be between 4 - 64 characters!", MinimumLength = 4)]
         [DataType(DataType.Text)]
         [Remote(action: "ValidateSignin", controller: "Validate", AdditionalFields = nameof(Password))]
+        [ModelBinder(typeof(TrimModelBinder))]
         public string? Name { get; set; }
 
         [Required(ErrorMessage = "Please enter a password!")]
         [StringLength(64, ErrorMessage = "Password must be between 4 - 64 characters!", MinimumLength = 4)]
         [DataType(DataType.Password)]
+        [ModelBinder(typeof(HashModelBinder))]
         public string? Password { get; set; }
     }
 }
--- TodoApp/ViewModels/SignupViewModel.cs
+++ TodoApp/ViewModels/SignupViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using TodoApp.ModelBinders;
 
 namespace TodoModels.ViewModels
 {
@@ -7,17 +9,20 @@ namespace TodoModels.ViewModels
         [Required(ErrorMessage = "Please enter a user name!")]
         [StringLength(64, ErrorMessage = "Name must be between 4 - 64 characters!", MinimumLength = 4)]
         [DataType(DataType.Text)]
+        [ModelBinder(typeof(TrimModelBinder))]
         public string? Name { get; set; }
 
         [Required(ErrorMessage = "Please enter a password!")]
         [StringLength(64, ErrorMessage = "Password must be between 4 - 64 characters!", MinimumLength = 4)]
         [DataType(DataType.Password)]
+        [ModelBinder(typeof(HashModelBinder))]
         public string? Password { get; set; }
         
         [Required(ErrorMessage = "Please confirm password!")]
         [StringLength(64, ErrorMessage = "Password must be between 4 - 64 characters!", MinimumLength = 4)]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Passwords must match!")]
+        [ModelBinder(typeof(HashModelBinder))]
         public string? ConfirmPassword { get; set; }
     }
 }
