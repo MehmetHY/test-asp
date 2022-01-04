2b4a5f371eed71c64b1044c773294db10715a615
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-04 22:16:11 +0300

    created sign up validation

---
+++ TodoApp/Extensions/Validation/SignupValidation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using TodoData.UnitOfWork;
+using TodoModels.ViewModels;
+
+namespace TodoApp.Extensions.Validation
+{
+    public static class SignupValidation
+    {
+        public static bool Valid(this ModelStateDictionary modelState, SignupViewModel model, UnitOfWork unitOfWork)
+        {
+            bool valid = true;
+
+            if (!modelState.IsValid)
+                valid = false;
+
+            if (unitOfWork.UserRepo.NameExists(model.Name!))
+                valid = false;
+
+            return valid;
+        }
+    }
+}
