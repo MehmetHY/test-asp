11ade8e4a24fefe0818d02685c0bdd7d929e9777
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-05 20:55:31 +0300

    created todo repository

---
+++ TodoData/Repository/TodoRepository.cs
@@ -0,0 +1,36 @@
+﻿using DbAccess.Data;
+using DbAccess.Data.Models;
+using DbAccess.Repository;
+using TodoModels.Models;
+
+namespace TodoData.Repository
+{
+    internal class TodoRepository : Repository<TodoModel>, ITodoRepository
+    {
+        public TodoRepository(IProcedureCaller pc) : base(pc)
+        {
+        }
+
+        public IEnumerable<TodoModel> GetOfCategory(int categoryId)
+        {
+            var parameters = new Dictionary<string, object?>
+            {
+                { nameof(categoryId), categoryId }
+            };
+            var sp = new StoredProcedureModel(typeof(TodoModel), nameof(GetOfCategory), parameters);
+            var result = _spCaller.GetRows<TodoModel>(sp);
+            return result;
+        }
+
+        public IEnumerable<TodoModel> GetOfUser(int userId)
+        {
+            var parameters = new Dictionary<string, object?>
+            {
+                { nameof(userId), userId }
+            };
+            var sp = new StoredProcedureModel(typeof(TodoModel), nameof(GetOfUser), parameters);
+            var result = _spCaller.GetRows<TodoModel>(sp);
+            return result;
+        }
+    }
+}
