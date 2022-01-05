3d0ae1e2b0b486df8750618658304f6fd32165f7
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-05 17:33:29 +0300

    created home view model factory

---
+++ TodoApp/ViewModels/Factories/HomeViewModelFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoData.UnitOfWork;
+using TodoApp.Extensions;
+
+namespace TodoApp.ViewModels.Factories
+{
+    public class HomeViewModelFactory
+    {
+        private readonly UnitOfWork _unitOfWork;
+        public HomeViewModelFactory(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public HomeViewModel CreateHomeViewModel(Controller controller)
+        {
+            var viewModel = new HomeViewModel();
+            int userId = controller.GetCurrentAccountId()!.Value;
+            viewModel.Load(userId, _unitOfWork);
+            return viewModel;
+        }
+    }
+}
