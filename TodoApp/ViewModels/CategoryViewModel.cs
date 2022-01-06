241c61ac9b75dcb37fdb7501d01f8ecaec897546
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-06 09:58:35 +0300

    created category view model

---
+++ TodoApp/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using TodoModels.Models;
+
+namespace TodoApp.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public CategoryModel? Category { get; set; } = null;
+        public CategoryModel? Parent { get; set; } = null;
+        public List<CategoryModel> Categories { get; set; } = new();
+        public List<TodoModel> Todos { get; set; } = new();
+    }
+}
