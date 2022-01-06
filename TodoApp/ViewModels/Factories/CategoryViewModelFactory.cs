97d1c1a48ff619521ce99f4333a9a492c1e2485f
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-06 10:06:46 +0300

    created category view model factory

---
+++ TodoApp/ViewModels/Factories/CategoryViewModelFactory.cs
@@ -0,0 +1,22 @@
+﻿using TodoData.UnitOfWork;
+
+namespace TodoApp.ViewModels.Factories
+{
+    public class CategoryViewModelFactory
+    {
+        private readonly UnitOfWork _unitOfWork;
+        public CategoryViewModelFactory(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public CategoryViewModel? Create(int? userId, int? rootCategoryId)
+        {
+            if (rootCategoryId == null || userId == null)
+                return null;
+
+            CategoryViewModel? categoryViewModel = new();
+            return categoryViewModel;
+        }
+    }
+}
