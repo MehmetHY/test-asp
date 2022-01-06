f9aed77b0b254cff9c29b3b8463dd67d7da452d5
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-06 23:02:46 +0300

    created error receiver action filter

---
+++ TodoApp/ActionFilters/ErrorReceiver.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace TodoApp.ActionFilters
+{
+    public class ErrorReceiver : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            var controller = context.Controller as Controller;
+            var modelState = controller?.ViewData.ModelState;
+
+            if (modelState != null)
+                return;
+
+            Dictionary<string, string>? errorDictionary;
+
+            if (!controller!.TempData.ContainsKey(nameof(errorDictionary)))
+                return;
+
+            errorDictionary = controller.TempData[nameof(errorDictionary)]
+                as Dictionary<string, string>;
+
+            if (errorDictionary == null || errorDictionary.Count > 0)
+                return;
+
+            foreach (var item in errorDictionary)
+                modelState!.AddModelError(item.Key, item.Value);
+        }
+    }
+}
