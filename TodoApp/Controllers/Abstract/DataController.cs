ab8b1dfcae1623e9428c6c5ea05a4a33dc01f053
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-08 10:29:18 +0300

    created data controller

---
+++ TodoApp/Controllers/Abstract/DataController.cs
@@ -0,0 +1,14 @@
+﻿using TodoApp.Services;
+using TodoData.UnitOfWork;
+
+namespace TodoApp.Controllers.Abstract
+{
+    public class DataController : BaseController
+    {
+        public UnitOfWork UnitOfWork { get; private set; }
+        public DataController(AppService service) : base(service)
+        {
+            UnitOfWork = service.UnitOfWork;
+        }
+    }
+}
