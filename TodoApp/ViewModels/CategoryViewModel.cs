9214fb01a502c840479dffdf9a7ac0e0afaea066
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-08 16:27:15 +0300

    reworked category partial view

--- TodoApp/ViewModels/CategoryViewModel.cs
+++ TodoApp/ViewModels/CategoryViewModel.cs
@@ -1,10 +1,16 @@
-﻿using TodoModels.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using TodoApp.ModelBinders;
+using TodoModels.Models;
 
 namespace TodoApp.ViewModels
 {
     public class CategoryViewModel
     {
         public int? Id { get; set; }
+        [DataType(DataType.Text)]
+        [StringLength(64, ErrorMessage = "Name must be between 4 - 64 characters!")]
+        [ModelBinder(typeof(TrimModelBinder))]
         public string? Name { get; set; }
         public int? UserId { get; set; }
         public int? BaseCategoryId { get; set; }
