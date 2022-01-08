46f720a34ad4d347909bf3ff1dc5e17a3ca8bd82
MehmetHY <mehmethakkiyildiz@yahoo.com>
2022-01-08 12:20:37 +0300

    created content view model

--- TodoApp/ViewModels/ContentViewModel.cs
+++ TodoApp/ViewModels/ContentViewModel.cs
@@ -4,5 +4,16 @@ namespace TodoApp.ViewModels
 {
     public class ContentViewModel
     {
+        public CategoryModel? Category { get; set; }
+        public List<CategoryModel> Categories { get; set; } = new();
+        public List<TodoModel> Todos { get; set; } = new();
+
+        public CreateCategoryViewModel CreateCategoryViewModel { get; set; } = new();
+        public UpdateCategoryViewModel UpdateCategoryViewModel { get; set; } = new();
+        public DeleteCategoryViewModel DeleteCategoryViewModel { get; set; } = new();
+        
+        public CreateTodoViewModel CreateTodoViewModel { get; set; } = new();
+        public UpdateTodoViewModel UpdateTodoViewModel { get; set; } = new();
+        public DeleteTodoViewModel DeleteTodoViewModel { get; set; } = new();
     }
 }
